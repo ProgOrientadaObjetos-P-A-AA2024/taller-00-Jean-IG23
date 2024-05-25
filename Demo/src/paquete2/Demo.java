71ecc55cc268c8cd00b27d40e407182c14233697
Jean-IG23 <116389739+Jean-IG23@users.noreply.github.com>
2024-05-25 17:52:28 -0500

    Update Demo.java

--- Demo/src/paquete2/Demo.java
+++ Demo/src/paquete2/Demo.java
@@ -4,6 +4,8 @@
  * and open the template in the editor.
  */
 package paquete2;
+import paquete3.Informacion;
+import paquete1.Mensaje;
 
 
 /**
@@ -16,8 +18,13 @@ public class Demo {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.printf(" %scon %d\n ",   Mensaje.obtenerMensaje()
+                , Informacion.obtenerHabitantes() );
         // usar printf;
+        
+        // importar los paquetes con las clases necesarias para que dentro de 
+        // un printf llamar a las funciones e imprimir la cadena formateada
+        // con los datos que corresponde y pide la orden del taller
     }
     
 }
